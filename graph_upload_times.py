445f74a3abe106a1bbd4efdbd048005106afd773
Derek Lam <dc.lam@mail.utoronto.ca>
2020-11-25 17:57:30 -0800

    Graphing + timezone-aware everything

--- graph_upload_times.py
+++ graph_upload_times.py
@@ -1,21 +1,32 @@
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
+import matplotlib as mpl
 
 from csv import reader
-from datetime import datetime
+import datetime as dt
+import dateutil.parser as dtp
+from dateutil import tz
+
+plt.ion()
+plt.clf()
+plt.show()
 
 with open("xkcd_upload_times.csv") as rf:
     data_x = []
     data_y = []
     for row in reader(rf):
         data_x.append(int(row[0]))
-        data_y.append(dates.date2num(datetime.fromisoformat(row[1]))%1)
+        data_y.append(
+        	dt.datetime.combine(
+        		dt.datetime.utcfromtimestamp(0),
+        		dtp.isoparse(row[1]).astimezone(tz=tz.gettz('America/New York')).time()
+        	)
+      	)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot_date(data_x, data_y, 'ro', xdate=False, ydate=True)
-ax.yaxis_date()
-fig.autofmt_xdate()
+plt.scatter(data_x, dates.date2num(data_y), marker='o', c='r')
+plt.xlabel('Comic number')
+plt.ylabel('Time of day (EST + DST)')
+plt.title('XKCD upload time of day vs. comic number')
+plt.gca().yaxis.set_major_formatter(mpl.dates.DateFormatter('%H:%M'))
 
-plt.show()
-datetime.fromisoformat(row[1])
+input()
